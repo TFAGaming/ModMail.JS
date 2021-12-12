89caa50bb7b69b68aeb6a6097195e449bf3acf08
TFA1 <92172698+TFAGaming@users.noreply.github.com>
2021-12-12 23:22:24 +0100

    Update
    Updated Console Log, like Index.ks

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+//This is server.js file where your bot will be hosted 24/7. Do not edit anything here or your bot will not work.
+
 const express = require('express');
 const server = express();
  
 server.all('/', (req, res) => {
-  res.send(`OK`)
+  res.send(`Bot is Ready! Your Bot is online 24/7.`)
 })
  
 function keepAlive() {
-  server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
+  server.listen(3000, () => { console.log("[CONSOLE: Replit]: Preparing the code... By: T.F.A#1887","\n-------------------------------------------\n[CONSOLE: Host]: Server.js is Loading... ")});
 }
  
 module.exports = keepAlive;
-
-//keep alive script was made by cpt.motchy
